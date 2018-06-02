48d36443a2ebf96ac06997ecea1f5a154c435d75
Aliaksandr Makavetski <Aliaksandr.Makavetski@thomsonreuters.com>
2018-06-02 22:18:19 +0300

    TriggerBuild2

--- src/main/java/com/epam/learning/simpleapp/SimpleappApplication.java
+++ src/main/java/com/epam/learning/simpleapp/SimpleappApplication.java
@@ -12,7 +12,7 @@ public class SimpleappApplication {
 
     @RequestMapping("/")
     public String hello(){
-        return "Hello, World!!!";
+        return "Hello, World!!!!!";
     }
 
     public static void main(String[] args) {
